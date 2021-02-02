e352e2e679be3aa9ad603f81b06ba5d90e9e522a
Chris Enzweiler <cdenzweiler@gmail.com>
2021-02-02 13:40:29 -0500

    Initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import './index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Welcome from './components/welcome/Welcome';
+import Clock from './components/clock/Clock';
+import Contact from './components/contact/Contact';
+import Navigation from './components/navigation/Navigation';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navigation />
+        <Switch>
+        <Route path="/clock" component={Clock} />
+        <Route path="/contact" component={Contact} />
+        <Route exact path="/welcome/:name" component={Welcome} />
+        <Route
+        exact
+          path="/"
+          render={(props) => <Welcome {...props} name="Chris" />}
+        />
+        <Route><div>404</div></Route>
+        
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/error.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Error() {
+    return(
+        <div className="404">
+            Error 404 - file not found
+        </div>
+    )
+}
+export default Error
